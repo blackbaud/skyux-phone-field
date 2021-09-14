c2b502b26843d3627503114a681de76bace3df16
Trevor Burch <Trevor.Burch@blackbaud.com>
2021-09-14 15:40:09 -0400

    Adopt Angular CLI

---
+++ projects/phone-field-showcase/src/app/visual/visual.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { SkyE2eThemeSelectorModule } from '@skyux/e2e-client';
+import { SkyInputBoxModule } from '@skyux/forms';
+import { SkyPhoneFieldModule } from '@skyux/phone-field';
+import { PhoneFieldVisualComponent } from './phone-field/phone-field-visual.component';
+import { VisualComponent } from './visual.component';
+import { SkyIdModule } from '@skyux/core';
+
+
+
+@NgModule({
+  declarations: [
+    PhoneFieldVisualComponent,
+    VisualComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    SkyE2eThemeSelectorModule,
+    SkyIdModule,
+    SkyInputBoxModule,
+    SkyPhoneFieldModule
+  ]
+})
+export class VisualModule { }
